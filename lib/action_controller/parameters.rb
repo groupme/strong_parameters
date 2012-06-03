9ce12723c09e10c92c2520a45085b922e758dabb
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-06-03 02:38:30 +0200

    use regexp to find multi-params keys

--- lib/action_controller/parameters.rb
+++ lib/action_controller/parameters.rb
@@ -103,9 +103,9 @@ module ActionController
       end
 
       def multi_param_keys_for(filter)
-        keys.select { |key|
-          key.index("#{filter}(") == 0 && key.index(")") == key.length - 1
-        }
+        keys.select do |key|
+          key.match /#{filter}\(\d+i\)/
+        end
       end
   end
 
